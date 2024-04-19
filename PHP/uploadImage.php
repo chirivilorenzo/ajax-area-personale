15b38ebc0f45dd6d76f0744fd098829375176375
chirivilorenzo <chirivilorenzo@gmail.com>
2024-04-19 09:53:07 +0200

    abilitare libreria GD per testare la resize

--- PHP/uploadImage.php
+++ PHP/uploadImage.php
@@ -7,10 +7,30 @@
         session_start();
     }
 
+
     if($_SERVER["REQUEST_METHOD"] === "POST"){
 
         $imageTmp = $_FILES["image"]["tmp_name"];
         $imageData = file_get_contents($imageTmp);
+
+
+        // Ridimensiona l'immagine
+        //serve abilitare la libreria GD 
+        $newWidth = 200; // Larghezza desiderata
+        $newHeight = 200; // Altezza desiderata
+        list($width, $height) = getimagesize($imageTmp);
+        $imageResized = imagecreatetruecolor($newWidth, $newHeight);
+        $image = imagecreatefromjpeg($imageTmp);
+        imagecopyresampled($imageResized, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+        // Salva l'immagine ridimensionata in un buffer
+        ob_start();
+        imagejpeg($imageResized);
+        $imageData = ob_get_clean();
+
+        // Chiudi l'immagine
+        imagedestroy($imageResized);
+
 
 
         $classeDB = new CDatabase();
